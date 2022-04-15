7c0f2b2281c3442189b6952829958a0a84605edf
Danilo Mello <danilodemellow@gmail.com>
2022-04-15 12:39:09 -0300

    chore: update jest config files for deploy

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,14 @@
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  setupFilesAfterEnv: [
-    "<rootDir>/src/tests/setup-tests.ts"
-  ],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setup-tests.ts"],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   moduleNameMapper: {
-    "\\.(scss|css|sass)$": "identity-obj-proxy"
-  }
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
+  },
+  "ts-jest": {
+    isolatedModules: false,
+  },
 };
